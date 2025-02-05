40e52869714afdc4e6f1a97cba6a4fd7998f658a
Jayden Kim <hkim@lcs.on.ca>
2025-02-05 10:20:55 -0500

    added example data

---
+++ GroceryStoreFlyer/Model/Flyer.swift
@@ -0,0 +1,76 @@
+//
+//  Flyer.swift
+//  GroceryStoreFlyer
+//
+//  Created by kim heejun on 2/5/25.
+//
+
+import Foundation
+
+struct Flyer {
+    let departments: [Department]
+}
+ 
+struct Department: Identifiable {
+    let id = UUID()
+    let name: String
+    let items: [FoodItem]
+}
+ 
+struct FoodItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let image: String
+}
+
+//creating an instance of these structures
+//in some cases, multiple instances
+let thisWeeksFlyer = Flyer(
+    
+    departments: [
+        
+        Department(
+            name: "Butcher's Best",
+            items: [
+                FoodItem(
+                    name: "Sirloin Tip Oven Roasts",
+                    image: "sirloin-tips"
+                ),
+                FoodItem(
+                    name: "Porkloin Back Ribs",
+                    image: "porkloin-back-ribs"
+                ),
+            ]
+        ),
+        
+        Department(
+            name: "Seafood",
+            items: [
+                FoodItem(
+                    name: "Wild Pacific Halibut Fillets",
+                    image: "wild-pacific-halibut-fillets"
+                ),
+                FoodItem(
+                    name: "Wild Sockeye Salmon Fillets",
+                    image: "wild-sockeye-salmon-fillets"
+                ),
+                FoodItem(
+                    name: "Clam Chowder",
+                    image: "clam-chowder"
+                ),
+            ]
+        ),
+        
+        
+    ]
+)
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
