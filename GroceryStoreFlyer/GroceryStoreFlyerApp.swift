493dbf06397a2715329a3663194a89587ce0d193
Jayden Kim <hkim@lcs.on.ca>
2025-02-05 09:48:12 -0500

    Initial Commit

---
+++ GroceryStoreFlyer/GroceryStoreFlyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreFlyerApp.swift
+//  GroceryStoreFlyer
+//
+//  Created by kim heejun on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFlyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
